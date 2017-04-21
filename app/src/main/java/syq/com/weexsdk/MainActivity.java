5b60ab24eadc777692f6118e8b6468ab16dd42d7
yanxianliang <yanxianliang@rongzi.com>
2017-04-21 13:06:18 +0800

    v 0.11.0 build

---
+++ app/src/main/java/syq/com/weexsdk/MainActivity.java
@@ -0,0 +1,17 @@
+package syq.com.weexsdk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ sdk/README.md
@@ -0,0 +1,8 @@
+## Unit Test
+
+Unit test code is under 'src/test'. You can run unit test use command:   
+```bash
+./gradlew clean testDebugUnitTest jacocoTestReportDebug
+```
+This task will run all unit test and produce both unit test report and jacoco test coverage report in 'build/reports'.   
+version 0.11
